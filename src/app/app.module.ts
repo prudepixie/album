4db8dbe52e4bbd06b7ee567d2ba515e8dff457c3
Wendy Hsu <wndyhsu@gmail.com>
2018-10-11 17:18:39 -0700

    scaffold and added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { CreateAlbumComponent } from './screens/album/create-album/create-album.component';
+import { HomeComponent } from './screens/home/home.component';
+import { UserComponent } from './screens/user/user.component';
+import { AppRoutingModule } from 'src/app/routes/routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CreateAlbumComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from 'src/app/screens/user/user.component';
+import { HomeComponent } from 'src/app/screens/home/home.component';
+import { CreateAlbumComponent } from 'src/app/screens/album/create-album/create-album.component';
+
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent},
+  { path: 'album', component: CreateAlbumComponent},
+  { path: '', component: HomeComponent},
+  { path: '**', redirectTo: ''}
+]
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [ RouterModule.forRoot(routes)]
+})
+
+
+export class AppRoutingModule { }
