abe3d778b20d8be105c158a10b35a6d73145fe35
prudepixie <wndyhsu@gmail.com>
2018-10-18 16:23:01 -0700

    wip

--- src/app/screens/albums/album/album.component.ts
+++ src/app/screens/albums/album/album.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-album',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    console.log('hi', this.route.snapshot.params)
   }
 
 }
